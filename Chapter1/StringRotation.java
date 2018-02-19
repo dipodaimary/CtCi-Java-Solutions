6deadc928c2ac2df26bb439cf9ee9af32f782fcb
dipodaimary <dipodaimary@gmail.com>
2018-02-19 23:45:00 +0530

    First Commit

---
+++ Chapter1/StringRotation.java
@@ -0,0 +1,16 @@
+package Chapter1;
+
+public class StringRotation {
+    public static void main(String[] args) {
+        System.out.println(isRotation("waterbottle", "erbottlewat"));
+    }
+
+    public static boolean isRotation(String s1, String s2) {
+        int len = s1.length();
+        if (len == s2.length() && len > 0) {
+            String s1s1 = s1 + s2;
+            return s1s1.contains(s2);
+        }
+        return false;
+    }
+}
