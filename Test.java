9b4cbb8bf219f4902feaff32e0e4badfaca4decd
dipodaimary <dipodaimary@gmail.com>
2018-02-20 00:43:53 +0530

    First Commit

--- Test.java
+++ Test.java
@@ -1,43 +1,39 @@
 public class Test {
-    private static int findLastCharacter(char[] s) {
-        for (int i = s.length - 1; i >= 0; i--) {
-            if (s[i] != ' ') {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private static void replaceSpaces(char[] arr, int trueLength) {
-        int spaceCount = 0, index, i = 0;
-        for (i = 0; i < trueLength; ++i) {
-            if (arr[i] == ' ') {
-                spaceCount++;
-            }
-        }
-        index = trueLength + spaceCount * 2;
-        if (trueLength < arr.length) {
-            arr[trueLength] = '\0';
-        }
-        for (i = trueLength - 1; i >= 0; i--) {
-            if (arr[i] == ' ') {
-                arr[index - 1] = '0';
-                arr[index - 2] = '2';
-                arr[index - 3] = '%';
-                index = index - 3;
-            } else {
-                arr[index - 1] = arr[i];
-                index--;
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        String str = "Mr John Smith    ";
-        char[] arr = str.toCharArray();
-        int trueLength = findLastCharacter(arr) + 1;
-        System.out.println(trueLength);
-        replaceSpaces(arr, trueLength);
-        System.out.println(arr);
+        LinkedListNode first = new LinkedListNode(0, null); //AssortedMethods.randomLinkedList(1000, 0, 2);
+        LinkedListNode head = first;
+        LinkedListNode second = first;
+        for (int i = 1; i < 8; i++) {
+            second = new LinkedListNode(i % 2, null);
+            first.setNext(second);
+            first = second;
+        }
+        System.out.println(head.printForward());
+    }
+}
+
+class LinkedListNode {
+    private int data;
+    private LinkedListNode next;
+
+    LinkedListNode() {
+
+    }
+
+    LinkedListNode(int d, LinkedListNode n) {
+        this.data = d;
+        this.next = n;
+    }
+
+    public void setNext(LinkedListNode node) {
+        this.next = node;
+    }
+
+    public String printForward() {
+        if (this.next != null) {
+            return this.data + "->" + next.printForward();
+        } else {
+            return ((Integer) this.data).toString();
+        }
     }
 }
