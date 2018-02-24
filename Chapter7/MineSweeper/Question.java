24097f6df5f4be343d21b8c4e21f66a39826088f
dipodaimary <dipodaimary@gmail.com>
2018-02-24 22:04:59 +0530

    First Commit

---
+++ Chapter7/MineSweeper/Question.java
@@ -0,0 +1,9 @@
+package Chapter7.MineSweeper;
+
+public class Question {
+    public static void main(String[] args) {
+        Game game = new Game(7, 7, 3);
+        game.initialize();
+        game.start();
+    }
+}
