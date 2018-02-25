54640fd8da37646528cc4f65d4e933a776e98c6d
dipodaimary <dipodaimary@gmail.com>
2018-02-26 02:19:47 +0530

    First Commit

---
+++ Chapter13/ObjectReflection/Rectangle.java
@@ -0,0 +1,15 @@
+package Chapter13.ObjectReflection;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double w, double h) {
+        width = w;
+        height = h;
+    }
+
+    public double area() {
+        return width * height;
+    }
+}
