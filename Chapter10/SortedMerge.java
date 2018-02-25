51bfaf230d91b55947f9e9e69d085dbcafb06651
dipodaimary <dipodaimary@gmail.com>
2018-02-25 23:12:41 +0530

    First Commit

---
+++ Chapter10/SortedMerge.java
@@ -0,0 +1,30 @@
+package Chapter10;
+
+public class SortedMerge {
+    public static void main(String[] args) {
+        int[] a = {2, 3, 4, 5, 6, 8, 10, 100, 0, 0, 0, 0, 0, 0};
+        int[] b = {1, 4, 7, 6, 7, 7};
+        sortedMerge(a, b, 8, 6);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+
+    public static void sortedMerge(int[] a, int[] b, int lastA, int lastB) {
+        int indexA = lastA - 1;
+        int indexB = lastB - 1;
+        int indexMerged = lastA + lastB - 1;
+        while (indexB >= 0) {
+            if (indexA >= 0 && a[indexA] > b[indexB]) {
+                a[indexMerged] = a[indexA];
+                indexA--;
+            } else {
+                a[indexMerged] = b[indexB];
+                indexB--;
+            }
+            indexMerged--;
+        }
+    }
+}
