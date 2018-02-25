318afc6e4690ed1af040fd4f32fabf605c811ce0
dipodaimary <dipodaimary@gmail.com>
2018-02-26 02:43:22 +0530

    First Commit

---
+++ Chapter13/LambdaExpression/Country.java
@@ -0,0 +1,25 @@
+package Chapter13.LambdaExpression;
+
+public class Country {
+    private String name;
+    private int size;
+    private String continent;
+
+    public Country(String n, String c, int sz) {
+        name = n;
+        size = sz;
+        continent = c;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public int getPopulation() {
+        return size;
+    }
+}
