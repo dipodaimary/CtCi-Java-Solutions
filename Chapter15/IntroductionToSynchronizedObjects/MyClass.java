281fcb832f5a139d86e3b222a24202e891bd9fc1
dipodaimary <dipodaimary@gmail.com>
2018-02-26 21:36:27 +0530

    First Commit

---
+++ Chapter15/IntroductionToSynchronizedObjects/MyClass.java
@@ -0,0 +1,15 @@
+package Chapter15.IntroductionToSynchronizedObjects;
+
+public class MyClass extends Thread {
+    private String name;
+    private MyObject myObj;
+
+    public MyClass(MyObject obj, String n) {
+        name = n;
+        myObj = obj;
+    }
+
+    public void run() {
+        myObj.foo(name);
+    }
+}
