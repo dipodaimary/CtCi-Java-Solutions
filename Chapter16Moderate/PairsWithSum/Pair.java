dd6e4f84f02cbed8f112bf57346605b45bb33898
dipodaimary <dipodaimary@gmail.com>
2018-02-28 00:05:46 +0530

    First Commit

---
+++ Chapter16Moderate/PairsWithSum/Pair.java
@@ -0,0 +1,16 @@
+package Chapter16Moderate.PairsWithSum;
+
+public class Pair {
+	public int first;
+	public int second;
+	
+	public Pair(int first, int second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	@Override
+	public String toString() {
+		return "(" + first + ", " + second + ")";
+	}
+}
