88c7a39b7966fe3908ccfc5466d829df09fd6052
dipodaimary <dipodaimary@gmail.com>
2018-02-27 23:25:42 +0530

    First Commit

---
+++ Chapter16Moderate/BisectSquare/Line.java
@@ -0,0 +1,15 @@
+package Chapter16Moderate.BisectSquare;
+
+public class Line {
+    public Point start;
+    public Point end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public String toString() {
+        return "Line from " + start.toString() + " to " + end.toString();
+    }
+}
