9957f3f8c7a5c40a3bdf9fabb8d28238670933a2
dipodaimary <dipodaimary@gmail.com>
2018-02-28 22:07:00 +0530

    First Commit

---
+++ Chapter17/Q17_MultiSearch/Trie.java
@@ -0,0 +1,19 @@
+//package Chapter17.Q17_MultiSearch;
+//
+//import java.util.ArrayList;
+//
+//public class Trie {
+//    private TrieNode root = new TrieNode();
+//
+//    public ArrayList<Integer> search(String s) {
+//        return root.search(s);
+//    }
+//
+//    public void insertString(String str, int location) {
+//        root.insertString(str, location);
+//    }
+//
+//    public TrieNode getRoot() {
+//        return root;
+//    }
+//}
