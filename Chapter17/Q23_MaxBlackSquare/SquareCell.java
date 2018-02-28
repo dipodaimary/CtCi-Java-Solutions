e9b7ed5b0760a7a420f8da41f9d1fbfbf5027ad8
dipodaimary <dipodaimary@gmail.com>
2018-02-28 22:43:12 +0530

    First Commit

---
+++ Chapter17/Q23_MaxBlackSquare/SquareCell.java
@@ -0,0 +1,19 @@
+package Chapter17.Q23_MaxBlackSquare;
+
+public class SquareCell {
+    public int zerosRight = 0;
+    public int zerosBelow = 0;
+
+    public SquareCell(int right, int below) {
+        zerosRight = right;
+        zerosBelow = below;
+    }
+
+    public void setZerosRight(int right) {
+        zerosRight = right;
+    }
+
+    public void setZerosBelow(int below) {
+        zerosBelow = below;
+    }
+}
