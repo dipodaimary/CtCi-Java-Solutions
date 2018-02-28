a4087c1f79e8973d920a27cfc20829330b6f60b5
dipodaimary <dipodaimary@gmail.com>
2018-02-28 21:51:52 +0530

    First Commit

---
+++ Chapter17/Q12_BiNode/BiNode.java
@@ -0,0 +1,11 @@
+package Chapter17.Q12_BiNode;
+
+public class BiNode {
+    public BiNode node1;
+    public BiNode node2;
+    public int data;
+
+    public BiNode(int d) {
+        data = d;
+    }
+}
