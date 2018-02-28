4f109f52414ca434fcc049a8abf874c3e570bf28
dipodaimary <dipodaimary@gmail.com>
2018-02-28 22:11:45 +0530

    First Commit

---
+++ Chapter17/Q18_Shortest_Subsequence/HeapNode.java
@@ -0,0 +1,16 @@
+package Chapter17.Q18_Shortest_Subsequence;
+
+public class HeapNode implements Comparable<HeapNode> {
+    public int locationWithinList;
+    public int listId;
+
+    public HeapNode(int location, int list) {
+        locationWithinList = location;
+        listId = list;
+    }
+
+    @Override
+    public int compareTo(HeapNode n) {
+        return locationWithinList - n.locationWithinList;
+    }
+}
---
+++ Chapter17/Q18_Shortest_Subsequence/Range.java
@@ -0,0 +1,27 @@
+package Chapter17.Q18_Shortest_Subsequence;
+
+public class Range {
+    private int start;
+    private int end;
+
+    public Range(int s, int e) {
+        start = s;
+        end = e;
+    }
+
+    public int length() {
+        return end - start + 1;
+    }
+
+    public boolean shorterThan(Range other) {
+        return length() < other.length();
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
