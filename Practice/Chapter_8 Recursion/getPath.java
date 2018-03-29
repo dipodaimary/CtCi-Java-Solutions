e81e55e7fce808857f1d0e224429853f9ed72407
dipodaimary <dipodaimary@gmail.com>
2018-03-29 23:59:13 +0530

    First Commit

--- Practice/Chapter_8 Recursion/getPath.java
+++ Practice/Chapter_8 Recursion/getPath.java
@@ -1,8 +1,13 @@
 package Recursion;
 
+import java.util.Arrays;
+
 public class getPath {
     public static void main(String[] args) {
         System.out.println(CountWaysRecursive(20));
+        int map[] = new int[20];
+        Arrays.fill(map, -1);
+        System.out.println(CountWaysMemoized(20, map));
     }
 
     public static int CountWaysRecursive(int n) {
@@ -12,6 +17,19 @@ public class getPath {
             return 1;
         } else {
             return CountWaysRecursive(n - 1) + CountWaysRecursive(n - 2) + CountWaysRecursive(n - 3);
+        }
+    }
+
+    public static int CountWaysMemoized(int n, int[] memo) {
+        if (n < 0) {
+            return 0;
+        } else if (n == 0) {
+            return 1;
+        } else if (memo[n] > -1) {
+            return memo[n];
+        } else {
+            memo[n] = CountWaysMemoized(n - 1, memo) + CountWaysMemoized(n - 2, memo) + CountWaysMemoized(n - 3, memo);
+            return memo[n];
         }
     }
 }
