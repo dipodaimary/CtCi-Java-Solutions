f6e10606cfb87aaea2ecc66d469ee63fad378ada
dipodaimary <dipodaimary@gmail.com>
2018-03-31 13:55:57 +0530

    First Commit

---
+++ Practice/Chapter_8 Recursion/Q6_Towers_Of_Hanoi.java
@@ -0,0 +1,62 @@
+package Recursion;
+
+import java.util.Stack;
+
+public class Q6_Towers_Of_Hanoi {
+    public static void main(String[] args) {
+        // Set up code.
+        int n = 5;
+        Tower[] towers = new Tower[3];
+        for (int i = 0; i < 3; i++) {
+            towers[i] = new Tower(i);
+        }
+        //Check before
+        for (int i = n - 1; i >= 0; i--) {
+            towers[0].add(i);
+        }
+
+        // Copy and paste output into a .XML file and open it with internet explorer.
+        //towers[0].print();
+        towers[0].moveDisks(n, towers[2], towers[1]);
+        //towers[2].print();
+    }
+}
+
+class Tower {
+    private Stack<Integer> disks;
+    private int index;
+
+    public Tower(int i) {
+        disks = new Stack<>();
+        index = i;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void add(int d) {
+        if (!disks.isEmpty() && disks.peek() <= d) {
+            System.out.println("Error placing disk " + d);
+        } else {
+            disks.push(d);
+        }
+    }
+
+    public void moveToTop(Tower t) {
+        int top = disks.pop();
+        t.add(top);
+    }
+
+    public void print() {
+        System.out.println("Contents of Tower_" + getIndex() + ": " + disks.toString());
+    }
+
+    public void moveDisks(int n, Tower destination, Tower buffer) {
+        if (n > 0) {
+            moveDisks(n - 1, buffer, destination);
+            moveToTop(destination);
+            buffer.moveDisks(n - 1, destination, this);
+        }
+    }
+}
